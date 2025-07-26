e2404e7e84c803e02bb23051a3c88f8a8bce4d38
Park Sang Min <sm0104224@gmail.com>
2025-07-26 21:44:03 +0900

    ec2-front update-13
    개인정보 페이지 수정 복구

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Home from './pages/Index';
+import Index from './pages/Index';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import Sell from './pages/Sell';
@@ -10,6 +10,7 @@ import Search from './pages/Search';
 import LikedProducts from './pages/LikedProducts';
 import MyPosts from './pages/MyPosts';
 import Profile from './pages/Profile';
+import ProfileSettings from './pages/ProfileSettings';
 import NotFound from './pages/NotFound';
 import EditPost from './pages/EditPost';
 import { AuthProvider } from './contexts/AuthContext';
@@ -24,7 +25,7 @@ function App() {
         <div className="min-h-screen bg-background">
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Index />} />
               <Route path="/products" element={<Products />} />
               <Route path="/product/:postPk" element={<ProductDetail />} />
               <Route path="/sell" element={<Sell />} />
@@ -32,6 +33,7 @@ function App() {
               <Route path="/liked-products" element={<LikedProducts />} />
               <Route path="/my-posts" element={<MyPosts />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/profile/settings" element={<ProfileSettings />} />
               <Route path="/edit-post/:postPk" element={<EditPost />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
